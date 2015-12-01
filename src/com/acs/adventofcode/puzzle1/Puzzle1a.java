480e6ab56f0913e2c81e177a44e0e77ad1396519
Peter Iskandar <bassingitup@gmail.com>
2015-12-01 13:53:23 -0500

    PeterI: completed puzzle1a and puzzle1b

---
+++ src/com/acs/adventofcode/puzzle1/Puzzle1a.java
@@ -0,0 +1,69 @@
+package com.acs.adventofcode.puzzle1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Puzzle1a {
+
+	private final static String INPUT_FILE_PATH = "/Users/devfloater56/Documents/mars_workspace/AdventOfCode/files/puzzle1/puzzle1input.txt";
+	
+	private final static int OPEN_PARENTHESES = '(';
+	private final static int CLOSE_PARENTHESES = ')';
+	
+	public static void main(String[] args)
+	{
+		
+		int floor = 0;
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		try {
+
+			fis = new FileInputStream(new File(INPUT_FILE_PATH));
+			isr = new InputStreamReader(fis);
+			
+			int i;
+			int position = 1;
+			while((i = isr.read()) != -1)
+			{
+				
+				if(i == OPEN_PARENTHESES){
+					floor++;
+				}else if (i == CLOSE_PARENTHESES){
+					floor--;
+				}
+				System.out.println(position);
+				
+			}
+			
+			System.out.println("floor: " + floor);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			if(isr != null){
+				try {
+					isr.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			if(fis != null){
+				try {
+					fis.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+//		
+		
+	}
+
+}
