52b9ee4b66f7136be5ecb6c470cad2f374e9ad54
Peter Iskandar <bassingitup@gmail.com>
2015-12-08 12:34:21 -0500

    completed puzzle 8a

---
+++ src/com/acs/adventofcode/puzzle8/tests/Puzzle8Tests.java
@@ -0,0 +1,28 @@
+package com.acs.adventofcode.puzzle8.tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.acs.adventofcode.puzzle8.Puzzle8;
+
+public class Puzzle8Tests {
+
+	Puzzle8 puzzle;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
