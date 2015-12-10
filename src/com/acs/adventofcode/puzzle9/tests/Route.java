17854aae1fa50cd3b84d8aed68aaf409293e7891
Peter Iskandar <bassingitup@gmail.com>
2015-12-10 18:04:30 -0500

    started puzzle 9

---
+++ src/com/acs/adventofcode/puzzle9/tests/Route.java
@@ -0,0 +1,29 @@
+package com.acs.adventofcode.puzzle9.tests;
+
+public class Route {
+
+	private String to;
+	private String from;
+	private int distance;
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	
+	
+	
+}
